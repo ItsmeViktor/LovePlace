654edf61154b6d099fe0d3d23b498723f835fcef
Виктор <itsmeviktor@mail.ru>
2022-04-03 01:20:10 +0300

    Custom View Cell

---
+++ LovePlace/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  LovePlace
+//
+//  Created by viktor on 03.04.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet var ImageOfPlace: UIImageView!
+    @IBOutlet var NameLabel: UILabel!
+    
+    @IBOutlet var TypeLabel: UILabel!
+    @IBOutlet var LocationLabel: UILabel!
+    
+}
